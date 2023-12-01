abd15828f6ca83b6237cbf378e2a908683a347b5
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-01 23:57:15 +0530

    search box

--- components/home/AddTask.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-
-const AddTask = () => {
-  return <SearchBar />;
-};
-
-export default AddTask;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
-import AddTask from "@/components/home/AddTask";
 import React from "react";
 import styles from "./homePage.module.css";
+import SearchBar from "@/components/home/SearchBar";
 
 const HomePage = () => {
   return (
     <div className={styles.container}>
-      <AddTask />
+      <SearchBar />
+      <hr
+        style={{
+          width: "75%",
+          height: "2px",
+          color: "black",
+          background: "black",
+        }}
+      />
     </div>
   );
 };
