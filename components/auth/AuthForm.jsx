5baf5e49c945b13b3dd2ffa4592193a6abe773ed
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-04 01:56:14 +0530

    added modal and show password feature

--- components/auth/AuthForm.jsx
+++ components/auth/AuthForm.jsx
@@ -2,17 +2,25 @@ import React, { useRef, useState } from "react";
 import styles from "./AuthForm.module.css";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
+import Modal from "../ui/Modal";
+import { RiEyeCloseFill, RiEyeFill } from "react-icons/ri";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [displayError, setDisplayError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [displayPassword, setDisplayPassword] = useState(false);
   const usernameRef = useRef();
   const passwordRef = useRef();
   const router = useRouter();
 
   const onChangeHandler = () => {
     setDisplayError(false);
+  };
+
+  const togglePasswordHandler = () => {
+    setDisplayPassword((prev) => !prev);
   };
 
   const toggleLoginSignupHandler = () => {
@@ -24,6 +32,7 @@ const AuthForm = () => {
 
   const submitHandler = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
 
     const enteredUsername = usernameRef.current.value;
     const enteredPassword = passwordRef.current.value;
@@ -62,54 +71,72 @@ const AuthForm = () => {
         setIsLogin(true);
       }
     }
+
+    setIsLoading(false);
   };
 
   return (
-    <form className={styles.authForm} onSubmit={submitHandler}>
-      <h1>{isLogin ? "LOGIN" : "SIGN UP"}</h1>
-      <div className={styles.inputField}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          required
-          ref={usernameRef}
-          onChange={onChangeHandler}
-          title="Please enter a username"
-        />
-      </div>
-      <div className={styles.inputField}>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          required
-          ref={passwordRef}
-          onChange={onChangeHandler}
-          title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
-          pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-        />
-      </div>
-      <button className={styles.actionButton}>
-        {isLogin ? "LOGIN" : "SIGN UP"}
-      </button>
-      {displayError && <p className={styles.errorMessage}>{errorMessage}</p>}
-      <p
-        onClick={toggleLoginSignupHandler}
-        className={styles.loginSignupToggle}
-      >
-        {isLogin ? (
-          <span>
-            New user?
-            <br /> <span className={styles.textBlue}>Click here</span> to create
-            an account!
-          </span>
-        ) : (
-          <span>
-            Already have an account? <br />
-            <span className={styles.textBlue}>Click here</span> to login!
-          </span>
-        )}
-      </p>
-    </form>
+    <>
+      {isLoading && (
+        <Modal message={`${isLogin ? "Logging" : "Signing"} you in...`} />
+      )}
+      <form className={styles.authForm} onSubmit={submitHandler}>
+        <h1>{isLogin ? "LOGIN" : "SIGN UP"}</h1>
+        <div className={styles.inputField}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            required
+            ref={usernameRef}
+            onChange={onChangeHandler}
+            title="Please enter a username"
+          />
+        </div>
+        <div className={styles.inputField}>
+          <label htmlFor="password">Password</label>
+          <input
+            type={displayPassword ? "text" : "password"}
+            required
+            ref={passwordRef}
+            onChange={onChangeHandler}
+            title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+          />
+          {displayPassword ? (
+            <RiEyeFill
+              className={styles.eyeIcon}
+              onClick={togglePasswordHandler}
+            />
+          ) : (
+            <RiEyeCloseFill
+              className={styles.eyeIcon}
+              onClick={togglePasswordHandler}
+            />
+          )}
+        </div>
+        <button className={styles.actionButton}>
+          {isLogin ? "LOGIN" : "SIGN UP"}
+        </button>
+        {displayError && <p className={styles.errorMessage}>{errorMessage}</p>}
+        <p
+          onClick={toggleLoginSignupHandler}
+          className={styles.loginSignupToggle}
+        >
+          {isLogin ? (
+            <span>
+              New user?
+              <br /> <span className={styles.textBlue}>Click here</span> to
+              create an account!
+            </span>
+          ) : (
+            <span>
+              Already have an account? <br />
+              <span className={styles.textBlue}>Click here</span> to login!
+            </span>
+          )}
+        </p>
+      </form>
+    </>
   );
 };
 
