f25b25ae33892e96c41f309727ce016fd8c6a674
Swapnil Mannurkar <mannurkarswapnil@gmail.com>
2023-12-06 22:38:54 +0530

    trial and error

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -19,21 +19,21 @@ const AuthenticationPage = () => {
   );
 };
 
-export const getServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
+// export const getServerSideProps = async (context) => {
+//   const session = await getServerSession(context.req, context.res, authOptions);
 
-  if (session) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
+//   if (session) {
+//     return {
+//       redirect: {
+//         destination: "/",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {},
-  };
-};
+//   return {
+//     props: {},
+//   };
+// };
 
 export default AuthenticationPage;
